dfa1c7f2c88fbebba05215850374bc92d6e967a3
OluRemiFour <lekanayoola04@gmail.com>
2024-08-28 03:56:05 -0700

    fix

--- app/layout.js
+++ app/layout.js
@@ -1,32 +1,8 @@
-// import { EB_Garamond } from "next/font/google";
-// import AppWrapper from "./AppWrapper";
-// import StoreContext from "./components/StoreContext";
-// import "./globals.css";
-// // import { StoreProvider } from "./components/StoreProvider";
-
-// const inter = EB_Garamond({ subsets: ["latin"] });
-
-// export const metadata = {
-//   title: "DLawNet",
-//   description: "Official website for Legal Education & Networking",
-// };
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body className={inter.className}>
-//         <AppWrapper>
-//           <StoreContext>{children}</StoreContext>
-//         </AppWrapper>
-//       </body>
-//     </html>
-//   );
-// }
-
 import { EB_Garamond } from "next/font/google";
 import AppWrapper from "./AppWrapper";
-import StoreProvider from "./components/storeProvider";
+// import StoreProvider from "./components/storeProvider";
 import "./globals.css";
+import StoreProvider from "./components/StoreProvider";
 
 const inter = EB_Garamond({ subsets: ["latin"] });
 
